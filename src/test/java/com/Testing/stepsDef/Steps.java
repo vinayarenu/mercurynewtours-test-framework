6299a63463e8e677c4b460bb42196bbc123d9901
vinaya <you@example.com>
2018-09-03 18:54:37 +0100

    Adding new scenario to book flight

--- src/test/java/com/Testing/stepsDef/Steps.java
+++ src/test/java/com/Testing/stepsDef/Steps.java
@@ -22,6 +22,10 @@ import static org.hamcrest.Matchers.endsWith;
 
 public class Steps {
     RegistrationPage registrationPage = new RegistrationPage();
+    FlightSearchPage flightSearchPage = new FlightSearchPage();
+    SelectFlightPage selectFlightPage = new SelectFlightPage();
+    BookAFlightPage bookAFlightPage = new BookAFlightPage();
+    FlightConfirmationPage flightConfirmationPage = new FlightConfirmationPage();
 
 
     @Before
@@ -69,6 +73,192 @@ public class Steps {
     public void user_should_successfully_register() {
         String actual = registrationPage.userSuccessfullyRegister();
         assertThat(actual, endsWith("success.php"));
+    }
+
+    @Given("^user selects flights$")
+    public void userSelectsFlights() {
+
+        flightSearchPage.selectFlights();
+    }
+
+    @Then("^user should navigate to FLIGHT FINDER page$")
+    public void userShouldNavigateToFLIGHTFINDERPage() {
+        String actual = flightSearchPage.isOnFlightPage();
+        assertThat(actual, endsWith("php"));
+    }
+
+    @When("^selects type \"([^\"]*)\"$")
+    public void selects_type(String type) {
+        flightSearchPage.selectType(type);
+    }
+
+    @When("^selects the passenger \"([^\"]*)\"$")
+    public void selects_the_passenger(String passenger) {
+        flightSearchPage.selectPassenger(passenger);
+
+    }
+
+    @When("^selects the departing from \"([^\"]*)\"$")
+    public void selects_the_departing_from(String departing) {
+        flightSearchPage.selectDepartingFrom(departing);
+
+
+    }
+
+    @When("^selects the month\"([^\"]*)\" and date\"([^\"]*)\"$")
+    public void selects_the_month_and_date(String month, String date) {
+        flightSearchPage.selectArrivingOn(month, date);
+
+    }
+
+    @When("^selects arriving destination \"([^\"]*)\"$")
+    public void selects_arriving_destination(String destination) {
+        flightSearchPage.selectArrivingDestination(destination);
+    }
+
+    @When("^selects returning month \"([^\"]*)\" and returning date \"([^\"]*)\"$")
+    public void selects_returning_month_and_returning_date(String returnMonth, String returnDate) {
+        flightSearchPage.selectReturnMonthDate(returnMonth, returnDate);
+    }
+
+    @When("^selects service class \"([^\"]*)\"$")
+    public void selects_service_class(String sClass) {
+        flightSearchPage.serviceClass(sClass);
+    }
+
+    @When("^selects airline \"([^\"]*)\"$")
+    public void selects_airline(String airline) {
+        flightSearchPage.selectAirline(airline);
+    }
+
+    @When("^selects continue$")
+    public void selects_continue() {
+        flightSearchPage.clickContine();
+    }
+
+    @Then("^user should navigate to SELECT FLIGHT PAGE$")
+    public void user_should_navigate_to_SELECT_FLIGHT_PAGE() {
+        String actual = selectFlightPage.isOnSelectFlightPage();
+        assertThat(actual, endsWith(".php"));
+    }
+
+
+    @Given("^selects continue button$")
+    public void selects_continue_button() {
+        selectFlightPage.selectContinue();
+    }
+
+    @Then("^user should navigate to BOOK A FLIGHT PAGE$")
+    public void user_should_navigate_to_BOOK_A_FLIGHT_PAGE() {
+        String actual = bookAFlightPage.isOnBookAFlightPage();
+        assertThat(actual, endsWith("mercurypurchase.php"));
+
+
+    }
+    @Then("^fills first name \"([^\"]*)\" from Passenger$")
+    public void fills_first_name_from_Passenger(String firstName) {
+        bookAFlightPage.passengerFirstname(firstName);
+    }
+
+
+    @Then("^fills last name \"([^\"]*)\" from Passenger$")
+    public void fills_last_name_from_Passenger(String lastName) {
+        bookAFlightPage.passengerLastName(lastName);
+
+    }
+
+    @Then("^selects meal \"([^\"]*)\" from Passenger$")
+    public void selects_meal_from_Passenger(String meals) {
+        bookAFlightPage.passengerMeals(meals);
+    }
+
+    @Then("^selects card type \"([^\"]*)\" from Credit Card$")
+    public void selects_card_type_from_Credit_Card(String cardType) {
+        bookAFlightPage.creditCardType(cardType);
+    }
+
+    @Then("^fills number \"([^\"]*)\" from Credit Card$")
+    public void fills_number_from_Credit_Card(String cardNumber) {
+        bookAFlightPage.creditCardNumber(cardNumber);
+    }
+
+    @Then("^selects expiration month \"([^\"]*)\" from Credit Card$")
+    public void selects_expiration_month_from_Credit_Card(String expMonth) {
+        bookAFlightPage.creditCardExpMonth(expMonth);
+    }
+
+    @Then("^selects expiration year \"([^\"]*)\" from Credit Card$")
+    public void selects_expiration_year_from_Credit_Card(String expYear) {
+        bookAFlightPage.creditCardExpYear(expYear);
+    }
+
+    @Then("^fills firstname \"([^\"]*)\" from Credit Card$")
+    public void fills_firstname_from_Credit_Card(String cardFirstName) {
+        bookAFlightPage.creditCardFirstName(cardFirstName);
+
+    }
+
+    @Then("^fills middle \"([^\"]*)\" from Credit Card$")
+    public void fills_middle_from_Credit_Card(String cardMiddleName) {
+        bookAFlightPage.creditCardMiddleName(cardMiddleName);
+    }
+
+    @And("^fills last \"([^\"]*)\" from Credit Card$")
+    public void fillsLastFromCreditCard(String cardLastName) {
+        bookAFlightPage.creditCardLastName(cardLastName);
+    }
+
+    @Then("^ticks Ticketless Travel check box from Billing address$")
+    public void ticks_Ticketless_Travel_check_box_from_Billing_address() {
+        bookAFlightPage.clickOnTicketLessCheckbox();
+    }
+
+    @Then("^fills address \"([^\"]*)\" from Billing Address$")
+    public void fills_address_from_Billing_Address(String address) {
+        bookAFlightPage.billingAddress(address);
+
+    }
+
+    @Then("^fills city \"([^\"]*)\"$")
+    public void fills_city(String city) {
+        bookAFlightPage.billingAddressCity(city);
+    }
+
+    @Then("^fills state and province \"([^\"]*)\"$")
+    public void fills_state_and_province(String state) {
+        bookAFlightPage.billingAddressState(state);
+    }
+
+    @Then("^fills postal code \"([^\"]*)\"$")
+    public void fills_postal_code(String postalCode) {
+        bookAFlightPage.billingAddressPostalCode(postalCode);
+
+    }
+
+    @Then("^selects country \"([^\"]*)\" from Billing address$")
+    public void selects_country_from_Billing_address(String country) {
+        bookAFlightPage.billingAddressCountry(country);
+    }
+
+    @Then("^ticks  Same as Billing Address check box from Delivery Address$")
+    public void ticks_Same_as_Billing_Address_check_box_from_Delivery_Address() {
+        bookAFlightPage.clickOnSameasBillingAddressCheckbox();
+    }
+
+    @Then("^selects SELECT PURCHASE button$")
+    public void selects_SELECT_PURCHASE_button() {
+        bookAFlightPage.clickOnSelectPurchaseBtn();
+    }
+
+    @Then("^user should navigate to Flight Confirmation page$")
+    public void user_should_navigate_to_Flight_Confirmation_page() {
+        String actual = flightConfirmationPage.isOnFlightConfirmationpage();
+        assertThat(actual, endsWith("php"));
+    }
+
+
+    @Then("^should view Your itinerary has been booked! message$")
+    public void should_view_Your_itinerary_has_been_booked_message() {
     }
 
 
--- src/test/resources/endToEnd.feature
+++ src/test/resources/endToEnd.feature
@@ -7,4 +7,42 @@ Feature: As a user i should be able to book a flight
     And selects submit
     Then user should successfully register
 
+  Scenario Outline: User should be able to book a flight journey with valid details
+    Given user selects flights
+    Then user should navigate to FLIGHT FINDER page
+    When selects type "<Type>"
+    And selects the passenger "<Passengers>"
+    And selects the departing from "<Departing From>"
+    And selects the month"<Arriving Month>" and date"<Arriving Date>"
+    And selects arriving destination "<Arriving In>"
+    And selects returning month "<Returning month>" and returning date "<Returning Date>"
+    And selects service class "<Service Class>"
+    And selects airline "<Airline>"
+    And selects continue
+    Then user should navigate to SELECT FLIGHT PAGE
+    And selects continue button
+    Then user should navigate to BOOK A FLIGHT PAGE
+    And fills first name "<First Name>" from Passenger
+    And fills last name "<Last Name>" from Passenger
+    And selects meal "<Meal>" from Passenger
+    And selects card type "<Card Type>" from Credit Card
+    And fills number "<Number>" from Credit Card
+    And selects expiration month "<Expiration Month>" from Credit Card
+    And selects expiration year "<Expiration year>" from Credit Card
+    And fills firstname "<First Name>" from Credit Card
+    And fills middle "<Middle>" from Credit Card
+    And fills last "<Last>" from Credit Card
+    And ticks Ticketless Travel check box from Billing address
+    And fills address "<Address>" from Billing Address
+    And fills city "<City>"
+    And fills state and province "<State/Province>"
+    And fills postal code "<Postal Code>"
+    And selects country "<Country>" from Billing address
+    And ticks  Same as Billing Address check box from Delivery Address
+    And selects SELECT PURCHASE button
+    Then user should navigate to Flight Confirmation page
+    And should view Your itinerary has been booked! message
+    Examples:
+      | Type      | Passengers | Departing From | Arriving Month | Arriving Date | Arriving In | Returning month | Returning Date | Service Class | Airline             | First Name | Last Name | Meal       | Card Type        | Number      | Expiration Month | Expiration year | Middle | Last | Address | City   | State/Province | Postal Code | Country        |
+      | RoundTrip | 1          | London         | May            | 2             | New York    | May             | 15             | Business      | Blue Skies Airlines | Vinaya     | M         | Vegetarian | American Express | 12345678910 | 01               | 2010            | D      | M    | Flat 2  | London | London         | IG11XR      | United Kingdom |
 
